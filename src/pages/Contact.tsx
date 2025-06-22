3750ab92ace10fbceccaf56acd92e3af17528391
tokyozen <77097819+tokyozen@users.noreply.github.com>
2025-06-22 06:05:54 +0100

    Updated Contact.tsx

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -18,7 +18,7 @@ const Contact = () => {
     {
       icon: <Phone className="w-6 h-6 text-muted-coral" />,
       title: "Phone",
-      details: ["(204) 825-8526"],
+      details: ["($$$"],
       action: "Call now"
     },
     {
